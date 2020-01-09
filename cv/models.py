7156355a6b0eedd7dad67d1c46f968d13818a8e7
Christopher Roy <christopher.roy@mail.mcgill.ca>
2020-01-08 19:46:24 -0500

    more work

--- cv/models.py
+++ cv/models.py
@@ -13,21 +13,52 @@ from django.db import models
 class EducationItem(models.Model):
     education_item_title = models.CharField(max_length=100)
     education_item_level = models.CharField(max_length=100, default="")
-    education_item_start_date = models.DateField(auto_now=True)
-    education_item_end_date = models.DateField(auto_now=True)
+    education_item_start_date = models.DateField(auto_now=False)
+    education_item_end_date = models.DateField(auto_now=False,null=True, blank=True)
     education_item_institution = models.CharField(max_length=100, default="")
 
+    def __str__(self):
+        tostr = "[" + self.education_item_title + "][" + \
+        self.education_item_level + "][" + \
+        self.education_item_institution + "]"
+
+        return tostr
 
 class WorkItem(models.Model):
     work_item_position_title = models.CharField(max_length=100)
     work_item_department = models.CharField(default="", null=True, max_length=100)
     work_item_company = models.CharField(max_length=100)
-    work_item_start_date = models.DateField(auto_now=True)
-    work_item_end_date = models.DateField(auto_now=True)
+    work_item_start_date = models.DateField(auto_now=False)
+    work_item_end_date = models.DateField(auto_now=False, null=True, blank=True)
+
+    @property
+    def get_bullets(self):
+        return WorkItemBullet.objects.filter(work_item=self).order_by('work_item_ordering')
 
     def __str__(self):
-        return self.work_item_position_title + " company: " + self.work_item_company
+        tostr = "[" + self.work_item_company + "][" + \
+                      self.work_item_position_title + "][" + \
+                      self.work_item_department + "]"
+        return tostr
+
+
 
 class WorkItemBullet(models.Model):
+
     work_item = models.ForeignKey(WorkItem, on_delete=models.CASCADE)
-    work_item_bullet = models.CharField(max_length=100)
+    work_item_bullet = models.TextField()
+    work_item_finished = models.BooleanField(default=False)
+    work_item_ordering = models.IntegerField()
+
+    def __str__(self):
+        tostr = "[" + str(self.work_item) + "][" + \
+        (str(self.work_item_bullet))[:100] + "][" + \
+        str(self.work_item_finished) + "]"
+
+        return tostr
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['work_item_ordering', 'work_item_finished'], name='ordering constraint'),
+            models.UniqueConstraint(fields=['work_item', 'work_item_bullet', 'work_item_finished'], name="duplicate bullet constraint")
+        ]
--- cv/views.py
+++ cv/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, HttpResponse
-from cv.models import WorkItem
+from cv.models import WorkItem, WorkItemBullet
 # Create your views here.
 def home(request):
     numbers = [1, 2, 3, 4, 5]
     name = 'Chris Roy'
 
-    work_experience = WorkItem.objects.all()
-    print(work_experience)
-    args = {'name': name, 'numbers': numbers, 'work_experience_items': work_experience}
+    work_experience_bullets = WorkItemBullet.objects.all()
+    work_items = WorkItem.objects.all()
+
+
+    args = {'name': name, 'numbers': numbers, \
+            'work_experience_items': work_items}
 
     return render(request, 'cv/home.html', args)
