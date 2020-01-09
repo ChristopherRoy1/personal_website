d65b334a3728eb12900f3bb694e8e30151688616
Christopher Roy <christopher.roy@mail.mcgill.ca>
2020-01-09 15:43:42 -0500

    added timeline for education + work

--- cv/views.py
+++ cv/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponse
-from cv.models import WorkItem, WorkItemBullet
+from cv.models import WorkItem, WorkItemBullet, EducationItem
 # Create your views here.
 def home(request):
     numbers = [1, 2, 3, 4, 5]
@@ -8,8 +8,10 @@ def home(request):
     work_experience_bullets = WorkItemBullet.objects.all()
     work_items = WorkItem.objects.all()
 
+    education_items = EducationItem.objects.all()
 
     args = {'name': name, 'numbers': numbers, \
-            'work_experience_items': work_items}
+            'work_experience_items': work_items, \
+            'education_items': education_items}
 
     return render(request, 'cv/home.html', args)
